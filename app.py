1aea338c5d1efe262f220d4ca59610caf8730554
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-01-25 21:23:47 +0000

    unable to deploy

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def index():
         
     return render_template("index.html", image_url=image_url, output_image=output_image)
 
-if  __name__ == "__main__":
-    app.run(debug=True)
+# if  __name__ == "__main__":
+#     app.run(debug=True)
