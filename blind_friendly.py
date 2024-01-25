229e01bd751d109ab75265e4e77e729228a72d68
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-01-25 17:00:58 +0000

    Ok pour les fonctionnalité séparé > combiné flask et la fonctionnalité

--- blind_friendly.py
+++ blind_friendly.py
@@ -1,20 +1,22 @@
 from image_processing import online_image 
-import os
 import matplotlib.pyplot as plt
 from daltonize import daltonize
 
-i = 1
+i = 2
 url = f"http://ty-ty.fr/assets/img/{i}.jpg"
 
-online_image = online_image(url)
-preprocessed_image = online_image.preprocess()
+def blind_friendly(url, output_name):
+    # Image loading
+    online_img = online_image(url)  # Changed variable name to avoid conflict
+    preprocessed_image = online_img.preprocess()
 
-fig = plt.imshow(preprocessed_image)
+    # Image processing to blind friendly
+    fig, ax = plt.subplots()
+    ax.imshow(preprocessed_image)
+    ax.axis('off') 
+    daltonized_fig = daltonize.daltonize_mpl(fig, copy=True)
+    daltonized_fig.savefig(f"{output_name}.png", bbox_inches='tight', pad_inches=0)
 
+    return "Image enregistrée !"
 
-fig, ax = plt.subplots()
-ax.imshow(preprocessed_image)
-daltonized_fig = daltonize.daltonize_mpl(fig, copy=True)
-
-
-daltonized_fig.savefig("output.png")
+blind_friendly(url, "image_2")
