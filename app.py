2899c8ec032d0a8b6307e471f40044ec531bfeb7
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-01-25 18:06:16 +0000

    ok for the project

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from blind_friendly import blind_optimized
 
 app = Flask(__name__)
 
-# Only one path for this case
-@app.route("/")
+@app.route("/", methods=('GET', 'POST'))
 def index(): 
-    return "Hello from flask"
+    image_url = None
+    output_image = None  # Initialize output_image here
+    if request.method == 'POST':
+        image_url = request.form.get('image_url')
+        output_image = blind_optimized(image_url, "image_2")
+        
+    return render_template("index.html", image_url=image_url, output_image=output_image)
 
 if  __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- blind_friendly.py
+++ blind_friendly.py
@@ -5,7 +5,7 @@ from daltonize import daltonize
 i = 2
 url = f"http://ty-ty.fr/assets/img/{i}.jpg"
 
-def blind_friendly(url, output_name):
+def blind_optimized(url, output_name):
     # Image loading
     online_img = online_image(url)  # Changed variable name to avoid conflict
     preprocessed_image = online_img.preprocess()
@@ -15,8 +15,8 @@ def blind_friendly(url, output_name):
     ax.imshow(preprocessed_image)
     ax.axis('off') 
     daltonized_fig = daltonize.daltonize_mpl(fig, copy=True)
-    daltonized_fig.savefig(f"{output_name}.png", bbox_inches='tight', pad_inches=0)
+    daltonized_fig.savefig(f"static/{output_name}.png", bbox_inches='tight', pad_inches=0)
 
-    return "Image enregistrée !"
+    return f"{output_name}.png"
 
-blind_friendly(url, "image_2")
+# blind_optimized("http://ty-ty.fr/assets/img/1.jpg", "image_2") # tesing
